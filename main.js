d7023a903a9eb423a3ca089c329a7c14ff08e757
andrearotesidev <andrearotesi@gmail.com>
2022-10-12 21:08:51 +0200

    Style revision, code optimization, added randomize function

--- main.js
+++ main.js
@@ -15,14 +15,35 @@ let radius = [
 
 code.innerHTML = "border-radius: " + radius.join(' ') + ";";
 rangeInputs.forEach((el, index) => {
-  el.addEventListener("input", (e) =>{
-    document.documentElement.style.setProperty("--radius-" + (index + 1), e.target.value + "%");
-    el.setAttribute("data-value", e.target.value + "%");
-    if(index === 4){
-      radius[index] = "/ " + e.target.value + "%";
-    }else{
-      radius[index] = e.target.value + "%";
-    }
-    code.innerHTML = "border-radius: " + radius.join(' ') + ";";
+  el.addEventListener("input", (e) => {
+    updateRadius(el, index, e.target.value);
   });
-})
+});
+
+function updateRadius(input, index, value) {
+  document.documentElement.style.setProperty("--radius-" + (index + 1), value + "%");
+  input.setAttribute("data-value", value + "%");
+  if (index === 4) {
+    radius[index] = "/ " + value + "%";
+  } else {
+    radius[index] = value + "%";
+  }
+  code.innerHTML = "border-radius: " + radius.join(' ') + ";";
+}
+
+/**
+ * Updates all inputs with a random value
+ */
+function randomizeBlob() {
+  rangeInputs.forEach((input, index) => {
+    updateRadius(input, index, getRandomValue());
+  });
+}
+
+/**
+ * Generates a random integer between 1 and 100
+ * @returns integer
+ */
+function getRandomValue() {
+  return Math.floor(Math.random() * (100 - 1));
+}
